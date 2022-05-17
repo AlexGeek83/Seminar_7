1ed2cf024791b242b67605c278788e15cd95c9c7
Alex <lrl.msk@yandex.ru>
2022-05-17 09:21:15 +0300

    add

--- Задача_2/Program.cs
+++ Задача_2/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задать двумерный массив размера m на n, каждый элемент в 
+массиве находится по формуле A(m,n) = m + n. Вывести массив на экран. */
+
+
+int m, n;
+System.Console.WriteLine("Введите кол-во строк (m): ");
+m = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите кол-во столбцов (n): ");
+n = int.Parse(Console.ReadLine());
+
+
+void FillArray(int[,]array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = i + j;  // присваиваем массиву новые элементы
+        }
+    }
+}
+
+// вывод на экран
+void PrintArray(int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            System.Console.Write(array[i,j] + " "); // выводим строку/столбец на экран
+        System.Console.WriteLine(); // переход на новую строку
+        
+    }
+}
+
+int[,] array = new int[m,n];
+
+FillArray(array);
+PrintArray(array);
+
+
+
